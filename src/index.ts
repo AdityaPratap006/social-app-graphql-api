bcab0dd72b67312ad6ba9decab16fd0e6e1b8382
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-03 02:14:26 +0530

    created a basic graphql server, changed tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import { config as dotenvConfig } from 'dotenv';
 import chalk from 'chalk';
+import { ApolloServer, gql, IResolvers } from 'apollo-server-express';
+import http from 'http';
 
 dotenvConfig();
 
@@ -12,8 +14,38 @@ app.get('/rest', (_req: Request, res: Response) => {
     });
 });
 
+// types query / mutation / subscription
+const typeDefs = gql`
+    type Query {
+        totalPosts: Int!
+    }
+`;
+
+// resolvers
+const resolvers: IResolvers<any, any> = {
+    Query: {
+        totalPosts: () => 42,
+    }
+};
+
+// graphql server
+const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+});
+
+// connect apollo server to a specific HTTP famework i.e: express
+apolloServer.applyMiddleware({
+    app: app,
+});
+
+const httpServer = http.createServer(app);
+
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     const localURL = `http://localhost:${PORT}`;
     console.log(`Server is ready at ${chalk.blueBright(localURL)}`);
+
+    const graphqlURL = `${localURL}${apolloServer.graphqlPath}`;
+    console.log(`GraphQL Server is ready at ${chalk.magentaBright(graphqlURL)}`);
 });
