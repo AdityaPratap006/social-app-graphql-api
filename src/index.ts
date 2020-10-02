c85d7f9a65678fd22ba4e1a76f2082bf8da28182
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-02 21:43:09 +0530

    set up a basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello Aditya!");
+import express, { Express, Request, Response } from 'express';
+import { config as dotenvConfig } from 'dotenv';
+import chalk from 'chalk';
+
+dotenvConfig();
+
+const app: Express = express();
+
+app.get('/rest', (req: Request, res: Response) => {
+    res.json({
+        data: 'you hit a rest endpoint',
+    });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    const localURL = `http://localhost:${PORT}`;
+    console.log(`Server is ready at ${chalk.blueBright(localURL)}`);
+});
