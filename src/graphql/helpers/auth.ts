809dd76225bec14c5cf22d5be5afbec14f3a2ba9
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-21 19:02:23 +0530

    removed serviceAccount json file import

--- src/graphql/helpers/auth.ts
+++ src/graphql/helpers/auth.ts
@@ -2,21 +2,20 @@ import firebaseAdmin, { ServiceAccount } from 'firebase-admin';
 import chalk from 'chalk';
 import { Request } from 'express';
 import { config as dotenvConfig } from 'dotenv';
-import serviceAccount from '../../config/firebaseServiceAccountKey.json';
 
 dotenvConfig();
 
 const serviceAccountParams = <ServiceAccount>{
-    type: process.env.FIREBASE_type || serviceAccount.type,
-    projectId: process.env.FIREBASE_projectId || serviceAccount.project_id,
-    privateKeyId: process.env.FIREBASE_privateKeyId || serviceAccount.private_key_id,
-    privateKey: process.env.FIREBASE_privateKey?.replace(/\\n/g, '\n') || serviceAccount.private_key,
-    clientEmail: process.env.FIREBASE_clientEmail || serviceAccount.client_email,
-    clientId: process.env.FIREBASE_clientId || serviceAccount.client_id,
-    authUri: process.env.FIREBASE_authUri || serviceAccount.auth_uri,
-    tokenUri: process.env.FIREBASE_tokenUri || serviceAccount.token_uri,
-    authProviderX509CertUrl: process.env.FIREBASE_authProviderX509CertUrl || serviceAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: process.env.FIREBASE_clientC509CertUrl || serviceAccount.client_x509_cert_url
+    type: process.env.FIREBASE_type,
+    projectId: process.env.FIREBASE_projectId,
+    privateKeyId: process.env.FIREBASE_privateKeyId,
+    privateKey: process.env.FIREBASE_privateKey?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_clientEmail,
+    clientId: process.env.FIREBASE_clientId,
+    authUri: process.env.FIREBASE_authUri,
+    tokenUri: process.env.FIREBASE_tokenUri,
+    authProviderX509CertUrl: process.env.FIREBASE_authProviderX509CertUrl,
+    clientC509CertUrl: process.env.FIREBASE_clientC509CertUrl
 };
 
 firebaseAdmin.initializeApp({
